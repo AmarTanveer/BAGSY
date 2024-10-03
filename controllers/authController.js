ab22c0bc3809efcadbf8e0e012a14a7c851fa1b0
Amar Tanveer <amartanveer@gmail.com>
2024-10-04 00:50:11 +0530

    Added login user function in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,51 @@
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
+const {generateToken} = require("../utils/generateToken");
+const cookieParser = require("cookie-parser");
 
-module.exports.registerUser = async function(req, res) {
-    
-    try {
-    const {fullname, email, password} = req.body;
-    const user = await userModel.findOne({email});
+module.exports.registerUser = async function (req, res) {
+  try {
+    const { fullname, email, password } = req.body;
+    const user = await userModel.findOne({ email });
     if (user) {
-        return res.status(500).send("User with the email already exists");
+      return res.status(500).send("User with the email already exists");
     }
 
     bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+      bcrypt.hash(password, salt, async function (err, hash) {
         if (err) {
-            return res.status(500).send(err.message);
-        } 
-        bcrypt.hash(password, salt, async function (err, hash) {
-            if (err) {
-                return res.status(500).send(err.message);
-            } 
-            const createdUser = await userModel.create({
-                fullname, email, password: hash
-            })
-            return res.status(200).send(createdUser)
-        })
-    })
-   
-    } catch(err) {
-        console.log(err.message)
-        
-    }
-}
+          return res.status(500).send(err.message);
+        }
+        const createdUser = await userModel.create({
+          fullname,
+          email,
+          password: hash,
+        });
+        return res.status(200).send(createdUser);
+      });
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports.loginUser = async function (req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(500).send("Wrong email or password");
+  }
+  const result = await bcrypt.compare(password, user.password);
+  
+  if (!result) {
+    return res.status(500).send("Wrong email or password");
+  } 
+  
+  const token = generateToken(user);
+  res.cookie("token", token);
+  return res.status(200).send({ message: "Login successful", token });
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
-
-const {registerUser} = require("../controllers/authController");
+const {registerUser, loginUser} = require("../controllers/authController");
 
 router.get("/", function (req, res) {
   res.send("user");
 });
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
