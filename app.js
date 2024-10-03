2a0226614e0aced313a0622cdc1e1e742a3e252d
Amar Tanveer <amartanveer@gmail.com>
2024-10-03 23:31:03 +0530

    Installed Tailwind css, created the home page, tried user registeration POST request

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db = require("./config/mongoose-connection");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
 const ownersRouter = require("./routes/ownersRouter");
+const indexRouter = require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,13 +18,11 @@ app.set("view engine", "ejs");
 
 console.log(process.env.NODE_ENV);
 
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/owners", ownersRouter);
 
-app.get("/", function(req, res){
-    res.send("hkjk");
-});
 
 app.listen(3000, function(){
     console.log("Running");
