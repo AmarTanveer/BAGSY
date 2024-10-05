095261438702f97a5e9309fce43d177f7ab157c2
Amar Tanveer <amartanveer@gmail.com>
2024-10-05 15:06:40 +0530

    Added cloud mongodb database

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/bagsy`)
+  .connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.87p73.mongodb.net/bagsy`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(function () {
     console.log("Mongoose Connected");
   })
